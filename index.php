4e3d5f30980826a63e154c087a6f00ab5c2fa3fd
kefin <kevinrs@stud.ntnu.no>
2016-02-14 19:24:49 -0600

    Update and add a global page counter.

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 		<meta charset="utf-8">
 		<style>
 			body { background-color: black; color: white; font-style: verdana, sans-serif; }
-			input[type=submit] { width: 25%; }
+			input[type=submit] { width: 20%; }
+			select { width: 20%; }
 			pre { background-color: rgba(235, 236, 228, 0.2); padding: 1vh 1vw 1vh 1vw; }
 			.vertical.space { height: 1vh; }
 		</style>
@@ -28,6 +29,20 @@
 					$disabled = "";
 				return "<input name=\"page\" type=\"submit\" value=\"$page\"$disabled>";
 			}
+			function getFileCountInDirectory($dir) {
+				return iterator_count(new FilesystemIterator($dir, FilesystemIterator::SKIP_DOTS)) - 1;
+			}
+			function createDropDownMenu($page, $to) {
+				$html = '<select name="page" onchange="this.form.submit();">';
+				for ($i = 0; $i < $to; ++$i) {
+					if ($i == $page)
+						$selected = ' selected';
+					else
+						$selected = '';
+					$html .= "<option$selected value=$i>$i</option>";
+				}
+				return $html . '</select>';
+			}
 			function doesPageExist($page) {
 				return file_exists("pages/$page");
 			}
@@ -47,9 +62,10 @@
 			$page = intval($page);
 
 			if (doesPageExist($page)) {
-				echo "<form>";
+				echo '<form>';
 				echo createButton($prev5, !doesPageExist("$prev5"));
 				echo createButton($prev, !doesPageExist("$prev"));
+				echo createDropDownMenu($page, getFileCountInDirectory('pages/'));
 				echo createButton($next, !doesPageExist("$next"));
 				echo createButton($next5, !doesPageExist("$next5"));
 				echo "</form>";
