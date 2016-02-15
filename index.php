79cab842b5946b13005e9497e7538a708577639d
kefin <kevinrs@stud.ntnu.no>
2016-02-15 02:33:12 -0600

    Clean up the code and fix a lot of css

--- index.php
+++ index.php
@@ -2,89 +2,50 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="reset.css">
 		<style>
+			.col-md-5 { width: 20%; }
+			.content { margin: 1vh 1vw; }
+			.prefoot { min-height: 95vh; }
+			.vertical.space { height: 1vh; }
 			body { background-color: black; color: white; font-style: verdana, sans-serif; }
 			form { display: inline; }
-			input[type=submit] { width: 20%; }
-			select { width: 20%; }
-			pre { background-color: rgba(235, 236, 228, 0.2); padding: 1vh 1vw 1vh 1vw; }
-			.vertical.space { height: 1vh; }
+			button { background: #232b30; border: 1px solid #1c252b; color: #9fa8b0; font-weight: bold; height: 3vh; outline: 0; width: 20%; }
+			button:hover { color: #fff; background: #4C5A64; }
+			select { border: 1px solid #1c252b; font-weight: bold; height: 3vh; width: 20%; }
+			pre { background-color: rgba(235, 236, 228, 0.2); font-family: monospace; margin: 1vh 0 1vh 0; padding: 1vh 1vw 1vh 1vw; }
 		</style>
 		<title>Cxy: The Fourth Evolution</title>
 	</head>
 	<body>
+		<div class="prefoot">
 		<?php
-			function getNext($page, $jump=1) {
-				return $page + $jump;
-			}
-			function getPrev($page, $jump=1) {
-				$prev = $page - $jump;
-				if ($prev <= 0)
-					return 0;
-				return $prev;
-			}
-			function createButton($page, $disabled) {
-				if ($disabled == true)
-					$disabled = " disabled";
-				else
-					$disabled = "";
-				return "<input name=\"page\" type=\"submit\" value=\"$page\"$disabled>";
-			}
-			function getFileCountInDirectory($dir) {
-				$result = array();
-				exec("ls pages/ | wc -l", $result);
-				return $result[0];
-			}
-			function createDropDownMenu($page, $to) {
-				$html = '<select name="page" onchange="this.form.submit();">';
-				for ($i = 0; $i < $to; ++$i) {
-					if ($i == $page)
-						$selected = ' selected';
-					else
-						$selected = '';
-					$html .= "<option$selected value=\"$i\">$i</option>";
-				}
-				return $html . '</select>';
-			}
-			function doesPageExist($page) {
-				return file_exists("pages/$page");
-			}
-			function getPage($page) {
-				return file_get_contents("pages/$page");
-			}
+			include_once 'nav/Nav.php';
 
 			$page = $_GET['page'];
 			if (!isset($page))
 				$page = 0;
 
-			$prev = getPrev($page);
-			$next = getNext($page);
-			$prev5 = getPrev($page, 5);
-			$next5 = getNext($page, 5);
-
 			$page = intval($page);
-
-			if (doesPageExist($page)) {
-				echo '<form>';
-				echo createButton($prev5, !doesPageExist("$prev5"));
-				echo createButton($prev, !doesPageExist("$prev"));
-				echo "</form>";
-				echo '<form>';
-				echo createDropDownMenu($page, getFileCountInDirectory('pages/'));
-				echo "</form>";
-				echo '<form>';
-				echo createButton($next, !doesPageExist("$next"));
-				echo createButton($next5, !doesPageExist("$next5"));
-				echo "</form>";
-				echo '<div class="vertical space"></div>';
-				echo getPage($page);
-			}
-			else
-				echo '
-					<form>
-						<input type="submit" name="page" value="0">
-					</form>
-					<pre> This part of the tutorial is not present </pre>';
+			echo generateNavibar($page);
+		?>
+			<div class="content">
+			<?php
+				if (doesPageExist($page)) {
+					echo getVerticalSpace();
+					echo getPage($page);
+				}
+				else {
+					echo generateNavibar($page);
+					echo '<div class="vertical space"></div>';
+					echo '<pre> This part of the tutorial is not present </pre>';
+				}
+			?>
+			</div>
+		</div>
+		<?php
+				echo getVerticalSpace();
+			echo generateNavibar($page);
 		?>
 	</body>
 </html>
---
+++ nav/Nav.php
@@ -0,0 +1,63 @@
+<?php
+	function getNext($page, $jump=1) {
+		return $page + $jump;
+	}
+	function getPrev($page, $jump=1) {
+		$prev = $page - $jump;
+		return $prev;
+	}
+	function createButton($page, $disabled, $text) {
+		if ($disabled == true)
+			$disabled = " disabled";
+		else
+			$disabled = "";
+		return "<button name=\"page\" type=\"submit\" value=\"$page\"$disabled>$text</button>";
+	}
+	function getFileCountInDirectory($dir) {
+		$result = array();
+		exec("ls pages/ | wc -l", $result);
+		return $result[0];
+	}
+	function createDropDownMenu($page, $to) {
+		$html = '<select name="page" onchange="this.form.submit();">';
+		for ($i = 0; $i < $to; ++$i) {
+			if ($i == $page)
+				$selected = ' selected';
+			else
+				$selected = '';
+			$html .= "<option$selected value=\"$i\">$i</option>";
+		}
+		return $html . '</select>';
+	}
+	function doesPageExist($page) {
+		return file_exists("pages/$page");
+	}
+	function getPage($page) {
+		return file_get_contents("pages/$page");
+	}
+	function getVerticalSpace() {
+		return '<div class="vertical space"></div>';
+	}
+	function generateNavibar($page) {
+		$prev = getPrev($page);
+		$next = getNext($page);
+		$prev5 = getPrev($page, 5);
+		$next5 = getNext($page, 5);
+
+		$page = intval($page);
+		$html = '';
+		$html .=  '<form>';
+		$html .=  createButton($prev5, !doesPageExist("$prev5"), '<<');
+		$html .=  createButton($prev, !doesPageExist("$prev"), '<');
+		$html .=  "</form>";
+		$html .=  '<form>';
+		$html .=  createDropDownMenu($page, getFileCountInDirectory('pages/'));
+		$html .=  "</form>";
+		$html .=  '<form>';
+		$html .=  createButton($next, !doesPageExist("$next"), '>');
+		$html .=  createButton($next5, !doesPageExist("$next5"), '>>');
+		$html .=  "</form>";
+		return $html;
+	}
+
+?>
